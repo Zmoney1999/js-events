ddf7e1db9fe987c179578a90b34b3fd5d3266eda
Zmoney1999 <91281730+Zmoney1999@users.noreply.github.com>
2021-11-04 14:27:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 window.addEventListener('click', function(){
-  console.log("== The window was clicked")
+  console.log("== The window was clicked") //When button is clicked it sends message to console
 
 })
 var buttons = document.getElementsByClassName('in-box-button')
 for (var i = 0; i < buttons.length; i++){
   buttons[i].addEventListener('click',function (){
-    console.log("== A button was clicked")
+    console.log("== A button was clicked")//When button is clicked it sends message to console
   })
+}
+function listenMaker(){
+  return function(){
+    console.log("== Inside listenMakers function")
+  }
+}
+function handleButtonClick(event){
+  console.log("== A button was clicked, event:", event)
+  var box = event.target.parentNode
+  box.classLst.toggle('highlighted')
 }
